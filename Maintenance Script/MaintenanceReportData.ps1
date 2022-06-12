6435fef9fa8b847226ae65fc3c914bd6bf5adfb9
Edward Laverty <laverty.eddie@gmail.com>
2022-06-12 02:03:07 -0400

    Adjusted Set-Content filepath.

--- Maintenance Script/MaintenanceReportData.ps1
+++ Maintenance Script/MaintenanceReportData.ps1
@@ -1,14 +1,14 @@
 $YYYYMMDD = Get-Date -format "yyyyMMdd"
 
 $model = Get-WmiObject Win32_ComputerSystem | Select-Object Manufacturer, Model
-$model | ConvertTo-Json | Set-Content "%~dp0\$YYYYMMDD-Logs\MaintenanceReportData.json"
+$model | ConvertTo-Json | Set-Content ".\$YYYYMMDD-Logs\MaintenanceReportData.json"
 $hostname = Get-WmiObject Win32_ComputerSystem | Select-Object Name
-$hostname | ConvertTo-Json | Add-Content "%~dp0\$YYYYMMDD-Logs\MaintenanceReportData.json"
+$hostname | ConvertTo-Json | Add-Content ".\$YYYYMMDD-Logs\MaintenanceReportData.json"
 $serial = Get-WmiObject Win32_bios | Select-Object SerialNumber
-$serial | ConvertTo-Json | Add-Content "%~dp0\$YYYYMMDD-Logs\MaintenanceReportData.json"
+$serial | ConvertTo-Json | Add-Content ".\$YYYYMMDD-Logs\MaintenanceReportData.json"
 $os = (Get-WmiObject -class Win32_OperatingSystem).Caption
-$os | ConvertTo-Json | Add-Content "%~dp0\$YYYYMMDD-Logs\MaintenanceReportData.json"
+$os | ConvertTo-Json | Add-Content ".\$YYYYMMDD-Logs\MaintenanceReportData.json"
 $ram = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb
-$ram | ConvertTo-Json | Add-Content "%~dp0\$YYYYMMDD-Logs\MaintenanceReportData.json"
+$ram | ConvertTo-Json | Add-Content ".\$YYYYMMDD-Logs\MaintenanceReportData.json"
 $drivespace = Get-CimInstance win32_logicaldisk | Where-Object caption -eq "C:" | foreach-object {Write-Output " $($_.caption) $('{0:N2}' -f ($_.Size/1gb)) GB total, $('{0:N2}' -f ($_.FreeSpace/1gb)) GB free "}
-$drivespace | ConvertTo-Json | Add-Content "%~dp0\$YYYYMMDD-Logs\MaintenanceReportData.json"
+$drivespace | ConvertTo-Json | Add-Content ".\$YYYYMMDD-Logs\MaintenanceReportData.json"
